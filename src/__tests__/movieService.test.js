d0c3b9cc81b924b7f34c825112aaf91a3483ea41
agonzalesbm <andres.gonzales.dev@gmail.com>
2026-02-02 21:30:41 -0400

    [@agonzalesbm] Added tests for searchMovies and getMovieDetails from movieService file

--- src/__tests__/movieService.test.js
+++ src/__tests__/movieService.test.js
@@ -31,6 +31,72 @@ describe("movieService", () => {
     });
 
     expect(fetch).toHaveBeenCalled();
-    await expect(movieService.getTrending()).rejects.toThrow();
+    await expect(movieService.getTrending()).rejects.toThrowError();
+  });
+
+  it("getMovieDetails should return movie details based on Id", async () => {
+    const mockData = {
+      "id": 19,
+      "overview": "In a futuristic city sharply divided...",
+      "title": "Metropolis",
+      "vote_average": 8.103,
+      "vote_count": 3023
+    };
+
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockData
+    });
+
+    const result = await movieService.getMovieDetails(19);
+
+    expect(result).toEqual(mockData);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it("getMovieDetails should throw an error when API response not ok", async () => {
+    fetch.mockResolvedValueOnce({
+      ok: false
+    });
+
+    expect(fetch).toHaveBeenCalled();
+    await expect(movieService.getMovieDetails()).rejects.toThrowError();
+  });
+
+  it("searchMovies shoudl return a movie based on a query", async () => {
+    const mockData = {
+      "results": [
+        {
+          "id": 195833,
+          "overview": "Vicente and Abigail run a banana...",
+          "title": "Caribe",
+          "vote_average": 5.7,
+        },
+        {
+          "id": 294244,
+          "overview": "Prepare to get tangled in the jungles of...",
+          "title": "Caribe",
+          "vote_average": 2.7,
+        },
+      ]
+    };
+
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockData
+    });
+
+    const result = await movieService.searchMovies("Caribe");
+
+    expect(result).toEqual(mockData);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it("searchMovies should throw an error when API response not ok", async () => {
+    fetch.mockResolvedValueOnce({
+      ok: false
+    });
+
+    await expect(movieService.searchMovies("Query")).rejects.toThrowError();
   });
 });
